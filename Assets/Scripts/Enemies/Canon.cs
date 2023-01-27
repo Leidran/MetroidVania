7224fec350d53add0d073991fddfbfe9e1ece687
unknown <jiaweiguo13@gmail.com>
2023-01-27 09:57:42 +0100

    canon enemy bug fixed (Only fire in range)

--- Assets/Scripts/Enemies/Canon.cs
+++ Assets/Scripts/Enemies/Canon.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject Bullet;
+    [SerializeField] float timeInterval = 3.0f;
+    private float currentTimer;
+    [SerializeField] private GameObject target;
+    [SerializeField] private float distance;
+    [SerializeField] private float range;
+    private void Awake()
     {
-        
+        shootPoint = GameObject.Find("ShootPoint").transform;
+        target = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //measure the distance between the current player position and enemy positon
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        //Fire the player if it is in range
+        if (distance < range)
+        {
+            Fire();
+        }
         
+    }
+    void Fire()
+    {
+        currentTimer -= Time.deltaTime;
+        if (currentTimer > 0) return;
+
+        currentTimer = timeInterval;
+        GameObject shootBullet = Instantiate(Bullet,shootPoint.position,shootPoint.rotation);
     }
 }
