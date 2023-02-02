2b7b14128650efb6c67e58151082901be1ab44ee
unknown <jiaweiguo13@gmail.com>
2023-02-02 13:09:44 +0100

    bat bug fixet

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -61,7 +61,7 @@ public class PlayerHealth : MonoBehaviour
         UIController.instance.UpdateUI(CurrentHealth, MaxHealth);
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
