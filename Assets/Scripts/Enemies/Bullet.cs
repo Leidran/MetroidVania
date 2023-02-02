e3f0e20016c780e2347680bae11c99dc1f806f8c
unknown <jiaweiguo13@gmail.com>
2023-02-02 13:16:15 +0100

    bullet bug fixed

--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -34,4 +34,8 @@ public class Bullet : MonoBehaviour
     {
         Destroy(gameObject);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
