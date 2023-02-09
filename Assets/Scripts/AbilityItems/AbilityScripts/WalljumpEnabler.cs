95f94950e0ecb36563ea6928ac93a5fd2a68040c
unknown <dgarciafx@gmail.com>
2023-02-09 12:30:25 +0100

    Floor layer agregada, per alguna raho no apareixia

--- Assets/Scripts/AbilityItems/AbilityScripts/WalljumpEnabler.cs
+++ Assets/Scripts/AbilityItems/AbilityScripts/WalljumpEnabler.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class WalljumpEnabler : MonoBehaviour
 {
+
+    Collider2D coll2D;
+    Rigidbody2D rb2D;
+
     void Start()
     {
-        
+        coll2D = GetComponent<Collider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
